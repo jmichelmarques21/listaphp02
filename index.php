e815412c62079227348507ee5485b5b1d2343105
jmichelmarques21 <jmichelmarques21@gmail.com>
2024-03-26 19:22:56 -0300

    commit index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lista Exercícios 02</title>
+</head>
+<body>
+
+<p><h1>Lista 2 de Exercicios em PHP</h1></p>
+<a href="exercicio01.php"><h3>Exercício 01</h3></a>
+<a href="exercicio02.php"><h3>Exercício 02</h3></a>
+<a href="exercicio03.php"><h3>Exercício 03</h3></a>
+<a href="exercicio04.php"><h3>Exercício 04</h3></a>
+<a href="exercicio05.php"><h3>Exercício 05</h3></a>
+<a href="exercicio06.php"><h3>Exercício 06</h3></a>
+<a href="exercicio07.php"><h3>Exercício 07</h3></a>
+<a href="exercicio08.php"><h3>Exercício 08</h3></a>
+<a href="exercicio09.php"><h3>Exercício 09</h3></a>
+<a href="exercicio10.php"><h3>Exercício 10</h3></a>
+<a href="exercicio11.php"><h3>Exercício 11</h3></a>
+<a href="exercicio12.php"><h3>Exercício 12</h3></a>
+<a href="exercicio13.php"><h3>Exercício 13</h3></a>
+<a href="exercicio14.php"><h3>Exercício 14</h3></a>
+<a href="exercicio15.php"><h3>Exercício 15</h3></a>
+<a href="exercicio16.php"><h3>Exercício 16</h3></a>
+<a href="exercicio17.php"><h3>Exercício 17</h3></a>
+<a href="exercicio18.php"><h3>Exercício 18</h3></a>
+<a href="exercicio19.php"><h3>Exercício 19</h3></a>
+<a href="exercicio20.php"><h3>Exercício 20</h3></a>
+<a href="exercicio21.php"><h3>Exercício 21</h3></a>
+<a href="exercicio22.php"><h3>Exercício 22</h3></a>
+<a href="exercicio23.php"><h3>Exercício 23</h3></a>
+<a href="exercicio24.php"><h3>Exercício 24</h3></a>
+<a href="exercicio25.php"><h3>Exercício 25</h3></a>
+<a href="exercicio26.php"><h3>Exercício 26</h3></a>
+<a href="exercicio27.php"><h3>Exercício 27</h3></a>
+<a href="exercicio28.php"><h3>Exercício 28</h3></a>
+<a href="exercicio29.php"><h3>Exercício 29</h3></a>
+<a href="exercicio30.php"><h3>Exercício 30</h3></a>
+  
+</body>
+</html>
