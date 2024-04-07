b0b3919a724dc902958de3878b64e07be2d0ee3f
jmichelmarques21 <jmichelmarques21@gmail.com>
2024-04-07 01:58:11 -0300

    commit exercicio18.php

---
+++ exercicio18.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercício 18</title>
+</head>
+<body>
+  <a href="index.php">Voltar</a>
+
+  <h3>Exercício 18 - Não está descrito no pdf...</h3>
+  <?php
+
+
+
+  ?>
+  
+</body>
+</html>
